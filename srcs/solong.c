b091b57ad7f2e7b26e53c719898678a87fae3864
ChrisJmz <christianjimenezgf@gmail.com>
2022-02-14 11:40:54 +0000

    showing map

--- srcs/solong.c
+++ srcs/solong.c
@@ -6,7 +6,7 @@
 /*   By: cjimenez <cjimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 11:29:51 by cmsjus            #+#    #+#             */
-/*   Updated: 2022/02/14 10:19:52 by cjimenez         ###   ########.fr       */
+/*   Updated: 2022/02/14 11:38:23 by cjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,22 @@
 int main(int ac, char **av)
 {
     t_data data;
+    int     i;
+    i = 0;
+    
     if (ac == 2)
     {
         data.map = ft_fill_map(av[1]);
         if (ft_file_name(av[1]) == 0 && ft_parse_map(data.map) == 0 && ft_content(data.map) == 0)
-            printf("TOUT EST OK\n");
+        {
+            printf("Map found :\n");
+            while (data.map[i] != 0)    
+            {
+                printf("%s", data.map[i]);
+                i++;
+            }
+            printf("\n");
+        }
     }
     else
     {
